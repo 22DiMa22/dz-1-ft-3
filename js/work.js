0e0d246f5c22af9000c96d5cb512244dcba94f57
22DiMa22 <129984472+22DiMa22@users.noreply.github.com>
2024-10-10 18:00:12 +0200

    dz 1 ft 3

---
+++ js/work.js
@@ -0,0 +1,32 @@
+const age = "15";
+console.log("age");
+
+const myName = "Dima";
+console.log(myName);
+
+const isStudent = "true";
+console.log(isStudent);
+
+const myString = "Не совершай классическую ошибку всех умников: не думай, что нет людей умнее тебя";
+console.log(myString);
+
+const myNumber = "0344444444";
+console.log(myNumber);
+
+const myNummber = 10;
+console.log(myNummber);
+
+const myNull = "null";
+console.log(myNull);
+
+const Name = prompt("Введіть сюди своє Ім'я");
+console.log("Name");
+
+const Сonfirm = confirm("Дякую за підтвердження!");
+console.log(Confirm);
+
+const Danger = alert("Дія небезпечна");
+console.log(Danger);
+
+const Conf = confirm("Дякую за підтвердження");
+console.log(Conf);
